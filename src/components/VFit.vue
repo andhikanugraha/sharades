ec90b5a822ff4f183a2f5b6d01a7162dc023b7b8
Andhika Nugraha <andhikanugraha@users.noreply.github.com>
2023-04-25 12:38:49 +0700

    Update VFit
    Use <script setup>

--- src/components/VFit.vue
+++ src/components/VFit.vue
@@ -2,25 +2,19 @@
   <h1 :style="{ fontSize }">{{ text }}</h1>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed } from 'vue';
+<script lang="ts" setup>
+import { computed } from 'vue';
 
-export default defineComponent({
-  name: 'VFit',
-  props: {
-    text: String,
-    width: Number,
-  },
-  setup(props) {
-    const fontSize = computed(() => {
-      const zoom = 1.1;
-      const width = (props.width || 100);
-      const characters = props.text?.length || 1;
-      const size: number = Math.min(Math.max(width / characters, 8), 22) * zoom;
-      return `${size}vw`;
-    });
+const props = defineProps<{
+  text: string,
+  width: number,
+}>();
 
-    return { fontSize };
-  },
+const fontSize = computed(() => {
+  const zoom = 1.1;
+  const width = (props.width || 100);
+  const characters = props.text?.length || 1;
+  const size: number = Math.min(Math.max(width / characters, 8), 22) * zoom;
+  return `${size}vw`;
 });
 </script>
